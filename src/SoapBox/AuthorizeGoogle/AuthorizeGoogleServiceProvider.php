42e2184a808486d8c3b36628eb75187f31ef9b0b
Jaspaul Bola <jaspaul.b@gmail.com>
2014-10-16 11:06:17 -0400

    Defer loading of our service provider, because performance

--- src/SoapBox/AuthorizeGoogle/AuthorizeGoogleServiceProvider.php
+++ src/SoapBox/AuthorizeGoogle/AuthorizeGoogleServiceProvider.php
@@ -10,7 +10,7 @@ class AuthorizeGoogleServiceProvider extends ServiceProvider {
 	 *
 	 * @var bool
 	 */
-	protected $defer = false;
+	protected $defer = true;
 
 	/**
 	 * Bootstrap the application events.
